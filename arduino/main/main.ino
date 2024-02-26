e15c76a282649f8baf5a0c9917e5a3ade4e3ec11
BingJiun Miu <98665601+joseph123223@users.noreply.github.com>
2024-02-26 09:59:26 -0800

    Add files via upload

---
+++ arduino/main/main.ino
@@ -0,0 +1,33 @@
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN 9
+#define SS_PIN  10
+byte readCard[4];
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+}
+
+void loop() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  for ( uint8_t i = 0; i < mfrc522.uid.size; i++) {  
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.println();  // End of UID
+
+  mfrc522.PICC_HaltA();
+  delay(1000);  // Add a delay
+}
